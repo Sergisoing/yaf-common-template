18b77501cbf736cbdea423293627d1ca68fc3db3
yangqinchuan <yangqinchuan@buding.cn>
2017-11-05 13:32:00 +0800

    add readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# Yaf-common-template
+Yaf + composer 公共模板， 方便快速创建项目
+g
--- application/controllers/Index.php
+++ application/controllers/Index.php
@@ -21,7 +21,7 @@ class IndexController extends Yaf\Controller_Abstract {
 		$model = new SampleModel();
 
 		//3. assign
-		$this->getView()->assign("content", $model->selectSample());
+		$this->getView()->assign("content", $05model->selectSample());
 		$this->getView()->assign("name", $name);
 
 		//4. render by Yaf, 如果这里返回FALSE, Yaf将不会调用自动视图引擎Render模板
